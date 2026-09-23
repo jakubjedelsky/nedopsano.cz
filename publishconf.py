5f2af86c0864565b12a2bdebe8e91b2443008c5e
Jakub Jedelský <jakub.jedelsky@gmail.com>
2026-09-23 15:52:00 +0200

    Let publishconf find pelicanconf outside build.sh

--- publishconf.py
+++ publishconf.py
@@ -1,4 +1,8 @@
-from pelicanconf import *  # noqa: F401,F403
+import os
+import sys
+
+sys.path.append(os.curdir)  # pelican loads this file by path, not as a module
+from pelicanconf import *  # noqa: E402,F401,F403
 
 SITEURL = "https://nedopsano.cz"
 RELATIVE_URLS = False
